d092a2a006c19ea0db38e477d20371a2b360e833
Krit Daowaset <kidrunner1@gmail.com>
2023-02-10 19:02:04 +0700

    update

--- lib/home.dart
+++ lib/home.dart
@@ -15,22 +15,37 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('MY NEON BUTTON'),
-      ),
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Column(
+        appBar: AppBar(
+          title: Text('MY NEON BUTTON'),
+        ),
+        backgroundColor: Colors.black,
+        body: Center(
+            child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             NeonButton(Colors.red, shadowColor: Colors.red),
-            NeonButton(Colors.red, shadowColor: Colors.red),
-            NeonButton(Colors.red, shadowColor: Colors.red),
-            NeonButton(Colors.red, shadowColor: Colors.red),
+            NeonButton(Colors.red, shadowColor: Colors.amber),
+            NeonButton(Colors.red, shadowColor: Colors.greenAccent),
+            NeonButton(Colors.red, shadowColor: Colors.white),
             NeonButton(Colors.red, shadowColor: Colors.blue),
+            SpinKitCircle(
+              size: 140,
+              itemBuilder: (context, index) {
+                final colors = [
+                  Colors.blue,
+                  Colors.red,
+                  Colors.amber,
+                  Colors.purple,
+                  Colors.green,
+                  Colors.orange
+                ];
+                final color = colors[index % colors.length];
+                return DecoratedBox(
+                    decoration:
+                        BoxDecoration(color: color, shape: BoxShape.circle));
+              },
+            )
           ],
-        ),
-      ),
-    );
+        )));
   }
 }
